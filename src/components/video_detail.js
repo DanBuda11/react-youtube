1ee1750eb502d0c8833029b19240715f3ac6fe42
Dan Buda <danbuda@gmail.com>
2017-03-30 17:18:29 -0500

    video select working

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -1,8 +1,13 @@
 import React from 'react';
 
 const VideoDetail = ({video}) => {
+	if (!video) {
+		return <div>Loading</div>
+	}
+
 	const videoId = video.id.videoId;
 	const url = `https://www.youtube.com/embed/${videoId}`;
+	
 	return (
 		<div className="video-detail col-md-8">
 			<div className="embed-responsive embed-responsive-16by9">
@@ -10,7 +15,7 @@ const VideoDetail = ({video}) => {
 			</div>
 			<div className="details">
 				<div>{video.snippet.title}</div>
-				<div>{video.snipet.description}</div>
+				<div>{video.snippet.description}</div>
 			</div>
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -2,22 +2,35 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoDetail from './components/video_detail';
 import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDpP9qh93VqmSKqOL0NpjH71hgm8u-lswI';
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		this.state = { videos: [] };
+
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		};
+
 		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-			this.setState({ videos });
+			this.setState({
+				videos: videos,
+				selectedVideo: videos[0]
+			});
 		});
 	}
+
 	render() {
 		return (
 			<div>
 				<SearchBar />
-				<VideoList videos={this.state.videos} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
 			</div>
 		);
 	}
