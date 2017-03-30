d47e322b0ae3060117c0194afe0609ecb699f6a0
Dan Buda <danbuda@gmail.com>
2017-03-30 17:00:16 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyDpP9qh93VqmSKqOL0NpjH71hgm8u-lswI';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { videos: [] };
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+			this.setState({ videos });
+		});
+	}
+	render() {
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+	}
+};
+
+ReactDOM.render(<App />, document.querySelector('.container'));
